832c1cc7fc186d383d9d398626eac511ff459832
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-11-04 15:50:11 +0000

    update: Navbar updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 export default function Navbar() {
     return (
-        <div className="px-5 py-3">
-            Navbar
-        </div>
-    )
+        <header className="navbar navbar-light px-5 py-3 border-bottom">
+            <nav className="d-flex justify-content-between align-items-center w-100">
+                <h1 className="mb-0">
+                    <span className="text-danger fw-bold cursor-pointer">ToDo</span>
+                </h1>
+                <ul className="list-unstyled d-flex gap-3 mb-0">
+                    <li><a href="#" className="fw-medium text-decoration-none link-hover">All Tasks</a></li>
+                    <li><a href="#" className="fw-medium text-decoration-none link-hover">Completed Tasks</a></li>
+                </ul>
+                <div className="d-flex gap-3">
+                    <button className="btn btn-danger">Log in</button>
+                    <button className="btn btn-danger">Sign up</button>
+                </div>
+            </nav>
+        </header>
+    );
 }
