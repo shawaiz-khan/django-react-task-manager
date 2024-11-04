e3df41560f0a3f1fbaaca1ba46bf94d3efc6112f
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-11-04 16:19:12 +0000

    update: Home page updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <main className='min-vh-100 bg-dark min-vw-100'>
-      <h1 className='text-bg-danger alert d-inline-block'>Hello World</h1>
+    <main className='min-vh-100 bg-light d-flex justify-content-center align-items-start p-5'>
+      <div className='w-100 d-flex justify-content-between align-items-center'>
+        <h1 className='text-center m-0 fs-3'>All Tasks</h1>
+        <div className='d-flex justify-content-center w-50'>
+          <input
+            type="text"
+            placeholder='Search for tasks...'
+            className='w-75 rounded border border-danger shadow-sm focus-ring p-2'
+          />
+          <button className='btn btn-danger ms-2'>Search</button>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
